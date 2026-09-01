bc90ee4e865c5ebc813d259ea11e9851a45c2471
soufiane ezzine <ezzine.soufiane@gmail.com>
2026-09-02 03:17:37 +0900

    add TCPPort type and mapping for common port names

--- internal/layers/tcp.go
+++ internal/layers/tcp.go
@@ -3,6 +3,8 @@ package layers
 import (
 	"encoding/binary"
 	"fmt"
+	"strconv"
+
 	lp "lazypacket"
 )
 
@@ -28,18 +30,28 @@ import (
 //		:                                                               |
 //		+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
-type TCP struct {
-	lp.BaseLayer
-	SrcPort, DstPort                       uint16
-	SeqNum                                 uint32
-	AckNum                                 uint32
-	DataOffset                             uint8
-	CWR, ECE, URG, ACK, PSH, RST, SYN, FIN bool
-	Window                                 uint16
-	Checksum                               uint16
-	UrgentPointer                          uint16
-	Options                                []TCPOption
-	Padding                                []byte
+type TCPPort uint16
+
+var tcpPortNames = map[TCPPort]string{
+	20:   "ftp-data",
+	21:   "ftp",
+	22:   "ssh",
+	23:   "telnet",
+	25:   "smtp",
+	53:   "dns",
+	80:   "http",
+	110:  "pop3",
+	143:  "imap",
+	443:  "https",
+	3306: "mysql",
+	5432: "postgresql",
+}
+
+func (tp TCPPort) String() string {
+	if name, ok := tcpPortNames[tp]; ok {
+		return fmt.Sprintf("%d (%s)", uint16(tp), name)
+	}
+	return strconv.Itoa(int(tp))
 }
 
 type TCPOptionKind uint8
@@ -81,6 +93,20 @@ type TCPOption struct {
 	OptionData   []byte
 }
 
+type TCP struct {
+	lp.BaseLayer
+	SrcPort, DstPort                       TCPPort
+	SeqNum                                 uint32
+	AckNum                                 uint32
+	DataOffset                             uint8
+	CWR, ECE, URG, ACK, PSH, RST, SYN, FIN bool
+	Window                                 uint16
+	Checksum                               uint16
+	UrgentPointer                          uint16
+	Options                                []TCPOption
+	Padding                                []byte
+}
+
 func (t *TCP) LayerType() lp.LayerType {
 	return lp.LayerTypeTCP
 }
@@ -91,8 +117,8 @@ func (t *TCP) DecodeFromBytes(data []byte, p lp.PacketBuilder) error {
 		return fmt.Errorf("[TCP] tcp header too small, only %d bytes minimum 20 bytes", len(data))
 	}
 
-	t.SrcPort = binary.BigEndian.Uint16(data[0:2])
-	t.DstPort = binary.BigEndian.Uint16(data[2:4])
+	t.SrcPort = TCPPort(binary.BigEndian.Uint16(data[0:2]))
+	t.DstPort = TCPPort(binary.BigEndian.Uint16(data[2:4]))
 	t.SeqNum = binary.BigEndian.Uint32(data[4:8])
 	t.AckNum = binary.BigEndian.Uint32(data[8:12])
 	t.DataOffset = data[12] >> 4
@@ -151,7 +177,8 @@ OPTIONS:
 			opt.OptionData = optionsData[2:length]
 		}
 
-		optionsData = optionsData[opt.OptionLength:]
+		optionsData = optionsData[opt.OptionLength:] // truncate to move to next
+		// this is incomplete but should be sufficient for now
 	}
 
 	return nil
