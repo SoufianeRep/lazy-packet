b87deba6a4f7f34816bf265ecc06d5a44c2c9991
soufiane ezzine <ezzine.soufiane@gmail.com>
2026-09-02 03:10:22 +0900

    enhance TCP struct with additional fields and option handling

--- internal/layers/tcp.go
+++ internal/layers/tcp.go
@@ -6,11 +6,79 @@ import (
 	lp "lazypacket"
 )
 
+//		0                   1                   2                   3
+//		0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+//		+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//		|          Source Port          |       Destination Port        |
+//		+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//		|                        Sequence Number                        |
+//		+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//		|                    Acknowledgment Number                      |
+//		+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//		|  Data |       |C|E|U|A|P|R|S|F|                               |
+//		| Offset| Rsrvd |W|C|R|C|S|S|Y|I|            Window             |
+//		|       |       |R|E|G|K|H|T|N|N|                               |
+//		+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//		|           Checksum            |         Urgent Pointer        |
+//		+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//		|                           [Options]                           |
+//		+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//		|                                                               :
+//		:                             Data                              :
+//		:                                                               |
+//		+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
 type TCP struct {
 	lp.BaseLayer
-	SrcPort uint16
-	DstPort uint16
-	SeqNum  uint32
+	SrcPort, DstPort                       uint16
+	SeqNum                                 uint32
+	AckNum                                 uint32
+	DataOffset                             uint8
+	CWR, ECE, URG, ACK, PSH, RST, SYN, FIN bool
+	Window                                 uint16
+	Checksum                               uint16
+	UrgentPointer                          uint16
+	Options                                []TCPOption
+	Padding                                []byte
+}
+
+type TCPOptionKind uint8
+
+const (
+	TCPOptionKindEndList       TCPOptionKind = 0
+	TCPOptionKindNop           TCPOptionKind = 1
+	TCPOptionKindMSS           TCPOptionKind = 2
+	TCPOptionKindWindowScale   TCPOptionKind = 3
+	TCPOptionKindSACKPermitted TCPOptionKind = 4
+	TCPOptionKindSACK          TCPOptionKind = 5
+	TCPOptionKindTimestamp     TCPOptionKind = 8
+)
+
+func (tok TCPOptionKind) String() string {
+	switch tok {
+	case TCPOptionKindEndList:
+		return "EndList"
+	case TCPOptionKindNop:
+		return "NOP"
+	case TCPOptionKindMSS:
+		return "MSS"
+	case TCPOptionKindWindowScale:
+		return "WindowScale"
+	case TCPOptionKindSACKPermitted:
+		return "SACKPermitted"
+	case TCPOptionKindSACK:
+		return "SACK"
+	case TCPOptionKindTimestamp:
+		return "Timestamp"
+	default:
+		return "Unsupported"
+	}
+}
+
+type TCPOption struct {
+	OptionType   TCPOptionKind
+	OptionLength uint8
+	OptionData   []byte
 }
 
 func (t *TCP) LayerType() lp.LayerType {
@@ -26,6 +94,65 @@ func (t *TCP) DecodeFromBytes(data []byte, p lp.PacketBuilder) error {
 	t.SrcPort = binary.BigEndian.Uint16(data[0:2])
 	t.DstPort = binary.BigEndian.Uint16(data[2:4])
 	t.SeqNum = binary.BigEndian.Uint32(data[4:8])
+	t.AckNum = binary.BigEndian.Uint32(data[8:12])
+	t.DataOffset = data[12] >> 4
+
+	t.CWR = data[13]&0x80 != 0
+	t.ECE = data[13]&0x40 != 0
+	t.URG = data[13]&0x20 != 0
+	t.ACK = data[13]&0x10 != 0
+	t.PSH = data[13]&0x08 != 0
+	t.RST = data[13]&0x04 != 0
+	t.SYN = data[13]&0x02 != 0
+	t.FIN = data[13]&0x01 != 0
+
+	t.Window = binary.BigEndian.Uint16(data[14:16])
+	t.Checksum = binary.BigEndian.Uint16(data[16:18])
+	t.UrgentPointer = binary.BigEndian.Uint16(data[18:20])
+
+	if t.DataOffset < 5 {
+		return fmt.Errorf("[TCP Error] invalid TCP DOffset %d < 5", t.DataOffset)
+	}
+
+	dataStart := int(t.DataOffset) * 4
+	if dataStart > len(data) {
+		p.SetTruncated()
+		t.Contents = data
+		t.Payload = nil
+		return fmt.Errorf("[TCP Error] truncated header: need %d bytes, got %d", dataStart, len(data))
+	}
+
+	t.Contents = data[:dataStart]
+	t.Payload = data[dataStart:]
+
+	optionsData := data[20:dataStart]
+OPTIONS:
+	for len(optionsData) > 0 {
+		t.Options = append(t.Options, TCPOption{OptionType: TCPOptionKind(optionsData[0])})
+		opt := &t.Options[len(t.Options)-1] // get the latest option
+		switch opt.OptionType {
+		case TCPOptionKindEndList:
+			opt.OptionLength = 1
+			t.Padding = optionsData[1:]
+			break OPTIONS
+		case TCPOptionKindNop:
+			opt.OptionLength = 1
+		default:
+			if len(optionsData) < 2 {
+				return fmt.Errorf("[TCP Error] truncated options, missing length byte")
+			}
+
+			length := optionsData[1]
+			if length < 2 || int(length) > len(optionsData) {
+				return fmt.Errorf("[TCP Error] invalid option length %d for kind %d", length, opt.OptionType)
+			}
+
+			opt.OptionLength = length
+			opt.OptionData = optionsData[2:length]
+		}
+
+		optionsData = optionsData[opt.OptionLength:]
+	}
 
 	return nil
 }
